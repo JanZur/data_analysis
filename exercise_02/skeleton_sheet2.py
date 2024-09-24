6e74ebb318f97cf6c0be7128076694fca9f662f6
JanZurb <91033413+JanZurb@users.noreply.github.com>
2024-09-24 10:48:24 +0200

    exercise 2 implementet first part

--- exercise_02/skeleton_sheet2.py
+++ exercise_02/skeleton_sheet2.py
@@ -16,29 +16,37 @@ def mean(x):
         The mean of the data.
     """
     # here goes your code
-    print("Calculating")  # replace this with your code
-    return 5  # replace this with your code
+
+
+    mean = np.sum(x, axis=0)/len(x)
+    numpy_mean = np.mean(x)
+    return mean
 
 
 def std(x):
     """Calculate the standard deviation for an array-like object x."""
     # here goes your code
-    print("Calculating")  # replace this with your code
-    return 5  # replace this with your code
+    std = np.sqrt(variance(x))
+    numpy_std = np.std(x)
+
+    return std
+
 
 
 def variance(x):
     """Calculate the variance for an array-like object x."""
     # here goes your code
-    print("Calculating")  # replace this with your code
-    return 5  # replace this with your code
+    var = np.sum(np.power(x - mean(x),2)) / (len(x)-1)
+    # with or without minus one numy does it without
+    numpy_var = np.var(x)
+    return var  # replace this with your code
 
 
 def mean_uncertainty(x):
     """Calculate the uncertainty in the mean for an array-like object x."""
     # here goes your code
-    print("Calculating")  # replace this with your code
-    return 5  # replace this with your code
+    mean_uncertainty = std(x)
+    return mean_uncertainty  # replace this with your code
 
 
 def ex1():
@@ -49,7 +57,8 @@ def ex1():
     mean_age = mean(age)
     mean_age_uncertainty = mean_uncertainty(age)
     # .2f means that the number is printed with two decimals. Check if that makes sense
-    print(f"The mean age of the participants is {mean_age:.2f} +/- {mean_age_uncertainty:.2f} years.")
+
+    print(f"The mean age of the participants is {mean_age:.1f} +/- {mean_age_uncertainty:.1f} years.")
 
 
 def ex2():
---
+++ playground/playground.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+print(np.power(5,2))
