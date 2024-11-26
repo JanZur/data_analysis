c54816581c4685375516d2c137bea88f8072267a
JanZurb <91033413+JanZurb@users.noreply.github.com>
2024-11-26 17:56:51 +0100

    up till 2f done

--- exercise_06/skeleton_sheet6.py
+++ exercise_06/skeleton_sheet6.py
@@ -10,81 +10,23 @@ You can directly use this sheet and modify it.
 import numpy as np
 import scipy.optimize as opt
 from matplotlib import pyplot as plt
+from numpy.core.fromnumeric import argmin
+from scipy.stats import chi2
 
 
 
 def load_data(filename):
-    """Load the data from a file.
-
-    Args:
-        filename (str): The name of the file to load.
-
-    Returns:
-        array: The data as a numpy array.
-    """
     return np.loadtxt(filename)
 
-
 def current_ohmslaw(U, R):
-    r"""Calculate the current according to Ohm's Law given the voltage U and resistance R.
-
-    Ohm's Law states that the current is given by:
-
-    .. math::
-
-        I = \frac{U}{R}
-
-    Args:
-        U (float, array): The measured voltage.
-        R (float, array): The resistance.
-
-    Returns:
-        float or array: Value of the linear function. Shape is the broadcast shape of
-            the inputs.
-    """
     return U / R
 
-
 def current_ohmslaw_bias(U, R, bias=None):
-    """Calculate the current according to Ohm's Law given the voltage U and resistance R with a bias.
-
-    Ohm's Law states that the current is given by:
-
-    .. math::
-
-        I = \frac{U}{R}
-
-    We can add a bias to the current by adding a constant to the voltage:
-
-    .. math::
-
-        I = \frac{U + bias}{R}
-
-    Args:
-        U (float, array): The measured voltage.
-        R (float, array): The resistance.
-        bias (float, array): The bias to add to the voltage. If None, no bias is added.
-
-    Returns:
-        float or array: Value of the linear function. Shape is the broadcast shape of
-            the inputs.
-    """
     if bias is None:
         bias = 0  # with this, we can also use the function without bias.
     return (U + bias) / R
 
-
-def chi2(x, y, err):
-    """Calculate the chi2 statistic for a dataset and its predictions.
-
-    Args:
-        x (array): The first data set.
-        y (array): Predicted values for the first data set.
-        err (array): The error on the measurements of the first data set.
-
-    Returns:
-        float: The chi2 statistic.
-    """
+def chi2_own_implementation(x, y, err):
     difference = np.array(np.abs(x - y))
     squared = np.square(difference)
     divided = squared  / np.square(err)
@@ -98,7 +40,6 @@ def optimal_resistance_analytical(measurements, uncertainties):
     data = measurements
     voltage = data[:, 0]
     current = data[:, 1]
-
     analytical_resistance = (np.mean(current * voltage) - np.mean(current) * np.mean(voltage)) / (
             np.mean(current * current) - np.mean(current) ** 2)
     return analytical_resistance
@@ -116,35 +57,29 @@ def ex_1a(measurements):
     # plt.show()
     plt.close()
 
-
-# This is an example of creating 1b composing different functions together.
-def chi2_1b(R, measurements, uncertainties):
-    """Calculate chi2 in dependence of the resistance."""
-
-    # Here is your code for exercise 1b.
+def chi2_1b(R, measurements, uncertainties, bias = None):
     data = measurements
-
     voltage = data[:, 0]
     current = data[:, 1]
-    # current_pred = current_ohmslaw(voltage, R)
-    current_pred = current_ohmslaw(voltage, R)
-    chi2val = chi2(current, current_pred, uncertainties)
+
+    current_pred = current_ohmslaw_bias(voltage, R, bias)
+    chi2val = chi2_own_implementation(current, current_pred, uncertainties)
     return chi2val
 
-
-def ex_1c(measurements, uncertainties):
-    """Run exercise 1c."""
-
-    # Here is your code for exercise 1c.
-    resistances = np.linspace(0.1, 10, num=1000)
-
-    # start, stop, number of steps
+def get_chi2_array(measurements, resistances, uncertainties, bias=None):
     chi2val = np.zeros(len(resistances))
     for i, R in enumerate(resistances):
-        chi2val[i] = chi2_1b(R, measurements, uncertainties)
+        chi2val[i] = chi2_1b(R, measurements, uncertainties, bias)
+    return chi2val
 
-    print(np.argmin(chi2val))
-    optimal_resistance = resistances[np.argmin(chi2val)]
+def get_optimal_resistance(resistances, chi2_array):
+    idx_min = argmin(chi2_array)
+    return resistances[idx_min]
+
+def ex_1c(measurements, uncertainties):
+    resistances = np.linspace(0.2, 10, num=1000)
+    chi2val = get_chi2_array(measurements, resistances, uncertainties)
+    optimal_resistance = get_optimal_resistance(resistances,chi2val)
 
     # plot the chi2 value as a function of the resistance.
     plt.figure()
@@ -176,45 +111,52 @@ def ex_1e(measurements, measurements_uncertainties):
     uncertainty_analytical_resistance = 1 / np.sqrt(np.sum(1 / measurements_uncertainties ** 2))
     print(
         f"The optimal analytical calculated resistance is {optimal_analytical_resistance:.2f} +- {uncertainty_analytical_resistance:.2f} Ohm.")
-    # get the uncertainty from the graph information
-    # when is chi2 = chi2 + 1
-    # get the resistances
-    resistances = np.linspace(1, 5, num=1000)
-    chi2val = np.zeros(len(resistances))
-    for i, R in enumerate(resistances):
-        chi2val[i] = chi2_1b(R, measurements, measurements_uncertainties)
 
-    min_chi2 = np.min(chi2val)
-    min_chi2_index = np.argmin(chi2val)
-    chi2_plus_1 = min_chi2 + 1
-    chi2_plus_1_index = np.argmin(np.abs(chi2val - chi2_plus_1))
-    resistance_plus_1 = resistances[chi2_plus_1_index]
-    uncertainty_resistance = np.abs(resistance_plus_1 - resistances[min_chi2_index])
-    chi_minus_1 = min_chi2 - 1
-    chi_minus_1_index = np.argmin(np.abs(chi2val - chi_minus_1))
-    resistance_minus_1 = resistances[chi_minus_1_index]
-    uncertainty_resistance_minus = np.abs(resistance_minus_1 - resistances[min_chi2_index])
+    resistances = np.linspace(1, 5, 1000)
+    chi2val = get_chi2_array(measurements,resistances, measurements_uncertainties)
+    min_chi2 = min(chi2val)
+    optimal_resistance = get_optimal_resistance(resistances, chi2val)
+
+    uncertainty_resistance, uncertainty_resistance_minus = get_deltachi_uncertainty(chi2val,min_chi2,resistances,optimal_resistance)
 
     print(
-        f"The optimal resistance is {resistances[min_chi2_index]:.2f} + {uncertainty_resistance:.2f} - "
+        f"The optimal resistance is {optimal_resistance:.2f} + {uncertainty_resistance:.2f} - "
         f"{uncertainty_resistance_minus:2f} Ohm.")
 
-def chi2_2a(R, measurements):
-    voltage = measurements[:, 0]
-    current = measurements[:, 1]
-    uncertainties_current = measurements[:, 2]
 
-    predictions = current_ohmslaw(voltage,R)
-    chi2_varying_uncertainties = chi2(current,predictions, uncertainties_current)
-    return chi2_varying_uncertainties
+def get_deltachi_uncertainty(chi2_values , chi2_min : float, resistances , optimal_resistance : float):
+    chi2_plus_1 = chi2_min + 1
+    indx_min = np.argmin(chi2_values)
+    chi2_values_below_min = chi2_values[:indx_min]
+    chi2_values_above_min = chi2_values[indx_min:]
+    resistance_below_min = resistances[:len(chi2_values_below_min)]
+    resistance_above_min = resistances[len(chi2_values_below_min):]
+
+    idx_below_min = argmin(np.abs(chi2_values_below_min-chi2_plus_1))
+    idx_above_min = argmin(np.abs(chi2_values_above_min-chi2_plus_1))
+    uncertainty_below = np.abs(optimal_resistance-resistances[idx_below_min])
+    uncertainty_above = np.abs(optimal_resistance-resistances[idx_above_min])
+
+    return uncertainty_below, uncertainty_above
+
+
+# def chi2_2a(R, measurements, bias = None):
+#     voltage = measurements[:, 0]
+#     current = measurements[:, 1]
+#     uncertainties_current = measurements[:, 2]
+#
+#     predictions = current_ohmslaw_bias(voltage,R, bias)
+#     chi2_varying_uncertainties = chi2(current,predictions, uncertainties_current)
+#     return chi2_varying_uncertainties
 
 
 
 def ex_2b(measurements):
     resistances = np.linspace(0.1, 5, num = 100)
-    chi2_varying_uncertainties = []
-    for R in resistances:
-        chi2_varying_uncertainties.append(chi2_2a(R, measurements))
+    uncertainties = measurements[:, 2]
+    chi2_varying_uncertainties = get_chi2_array(measurements, resistances, uncertainties)
+    # for R in resistances:
+    #     chi2_varying_uncertainties.append(chi2_1b(R, measurements,uncertainties))
 
     plt.figure()
     plt.plot(resistances, chi2_varying_uncertainties)
@@ -223,7 +165,7 @@ def ex_2b(measurements):
     plt.xlim(0, 5)
     plt.ylim(0, 1000)
     plt.savefig("ex2b.png")
-    plt.show()
+    #plt.show()
     plt.close()
 
     minimal_chi2 = np.min(chi2_varying_uncertainties)
@@ -233,13 +175,15 @@ def ex_2b(measurements):
     print(f"The optimal resistance is {optimal_resistance:.2f} Ohm.")
     print(f"The minimal chi^2 value is {minimal_chi2:.2f}.")
 
-    return optimal_resistance
+    #return optimal_resistance, chi2_varying_uncertainties
 
 def ex_2c(measurements):
-    optimal_resistance = ex_2b(measurements)
+    resistances = np.linspace(0.1, 5, num = 100)
+    chi2_array = get_chi2_array(measurements,resistances, measurements[:,2])
+    optimal_resistance = get_optimal_resistance(resistances, chi2_array)
     plt.figure()
     plt.plot(measurements[:, 0], measurements[:, 1], "o", label="Data")
-    plt.plot(measurements[:, 0], current_ohmslaw(measurements[:, 0], optimal_resistance), label="Fit")
+    plt.plot(measurements[:, 0], current_ohmslaw_bias(measurements[:, 0], optimal_resistance), label="Fit")
     plt.xlabel("Voltage [V]")
     plt.ylabel("Current [A]")
     plt.xlim(0, 15)
@@ -252,22 +196,80 @@ def ex_2c(measurements):
           "Otherwise it woul be possible to get a better fit by adding a bias to the model.")
 
 def ex_2d(measurements):
-    #find the errors on the resistance
-    pass
+    resistances = np.linspace(0.1, 5, num=100)
+    chi2_array = get_chi2_array(measurements, resistances, measurements[:, 2])
+    optimal_resistance = get_optimal_resistance(resistances, chi2_array)
+
+    uncertainty_resistance, uncertainty_resistance_minus = get_deltachi_uncertainty(chi2_array, np.min(chi2_array),
+                                                                                    np.linspace(
+        0.1, 5, num=100), optimal_resistance)
+    print(f"The uncertainties are: {uncertainty_resistance:.2f} and {uncertainty_resistance_minus:.2f} Ohm.")
+    print("These values are smaller than the given 2 Ohm and therefore is not compatible.")
+
+def ex_2e(measurements):
+    #use the offset function for prediciton
+    resistances = np.linspace(0.1, 5, num=100)
+    chi2_array = get_chi2_array(measurements, resistances, measurements[:, 2],0.7)
+    optimal_resistance = get_optimal_resistance(resistances, chi2_array)
+    uncertainty_upper, uncertainty_lower = get_deltachi_uncertainty(chi2_array,min(chi2_array),resistances, optimal_resistance)
+
+    print(f"When using the bias of 0.7 we get that the optimal resistance is: {optimal_resistance} + "
+          f"{uncertainty_upper} - {uncertainty_lower}")
+    print("This is still not compatible with the given 2 Ohm uncertainty.")
+
+    #plot the line to reassure it makes sense
+    # plt.figure()
+    # plt.plot(measurements[:, 0], measurements[:, 1], "o", label="Data")
+    # plt.plot(measurements[:, 0], current_ohmslaw_bias(measurements[:, 0], optimal_resistance,                                              0.7), label="Fit with bias")
+    # plt.xlabel("Voltage [V]")
+    # plt.ylabel("Current [A]")
+    # plt.xlim(0, 15)
+    # plt.ylim(0, 10)
+    # plt.legend()
+    # plt.show()
+    # plt.close()
+
+def chi2_ndf(chi2_values, ndf):
+    return chi2_values / ndf
+
+def ex_2f(measurements):
+    resistances = np.linspace(0.1, 5, num=100)
+    chi2_array_bias = get_chi2_array(measurements, resistances, measurements[:, 2],0.7)
+    chi2_array_no_bias = get_chi2_array(measurements, resistances, measurements[:, 2])
+
+    chi2_ndf_bias = chi2_ndf(min(chi2_array_bias), len(measurements)-2)
+    chi2_ndf_no_bias = chi2_ndf(min(chi2_array_no_bias), len(measurements)-1)
+
+    print("The chi2 ndf with the bias is: ", chi2_ndf_bias)
+    print("The chi2 ndf without the bias is: ", chi2_ndf_no_bias)
+    print("This shows that the one with the bias as an additional parameter works better.")
+
+    goodness_with_bias = chi2.cdf(chi2_ndf_bias * len(measurements - 2), len(measurements) - 2)
+    goodness_without_bias = chi2.cdf(chi2_ndf_no_bias * len(measurements - 1), len(measurements) - 1)
+
+
+    print("The goodness of fit with the bias is: ", goodness_with_bias)
+    print("The goodness of fit without the bias is: ", goodness_without_bias)
+
+    
 
 
 
-# def ex_2g():
-#     """Run exercise 2g."""
-#     # Here we need to use scipy.optimize.curve_fit to fit the data.
-#     # make sure to first read the documentation for curve_fit
-#     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
-#
-#     # NOTE: curve_fit already calculates the chi2 value (including error) for us!
-#     # hint: maybe look for simple examples around or play around if it is not clear on how to use curve_fit.
-#
-#     popt, pcov = opt.curve_fit(...)
-#     print("ex2g executed.")
+def ex_2g(measurements):
+    """Run exercise 2g."""
+    # Here we need to use scipy.optimize.curve_fit to fit the data.
+    # make sure to first read the documentation for curve_fit
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
+
+    # NOTE: curve_fit already calculates the chi2 value (including error) for us!
+    # hint: maybe look for simple examples around or play around if it is not clear on how to use curve_fit.
+
+    popt, pcov = opt.curve_fit(current_ohmslaw_bias, measurements[:, 0], measurements[:, 1], sigma=measurements[:, 2])
+    print("The optimal resistance is: ", popt[0])
+    print("The offset is : ", popt[1])
+    print("The uncertainty on the resistance is: ", np.sqrt(pcov[0,0]))
+
+
 
 
 if __name__ == '__main__':
@@ -299,7 +301,11 @@ if __name__ == '__main__':
     ex_2c(measurements)
     print("d)")
     ex_2d(measurements)
-
-
-    #ex_2g()
+    print("e)")
+    ex_2e(measurements)
+    print("f)")
+    ex_2f(measurements)
+    print("g) TODO")
+    ex_2g(measurements)
+    print("h) TODO")
 
